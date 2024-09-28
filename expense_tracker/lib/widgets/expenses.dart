81c7eb1343f62f6f386179b119f5f94feff641fa
jyroball <jyrojimenez@gmail.com>
2024-09-28 02:32:24 -0700

    Added pop up of keyboard function for title input

--- expense_tracker/lib/widgets/expenses.dart
+++ expense_tracker/lib/widgets/expenses.dart
@@ -1,4 +1,5 @@
 import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
+import 'package:expense_tracker/widgets/new_expense.dart';
 import 'package:flutter/material.dart';
 import 'package:expense_tracker/models/expense.dart';
 
@@ -27,10 +28,26 @@ class _ExpensesState extends State<Expenses> {
     ),
   ];
 
+  void _openAddExpenseOverlay() {
+    //show modal bottom sheet adds pop up at the bottom (maybe pop up a keyboard?)
+    showModalBottomSheet(
+      context: context, 
+      builder: (ctx) => const NewExpense(),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Expense Tracker'),
+        actions: [
+          IconButton(
+            onPressed: _openAddExpenseOverlay, 
+            icon: const Icon(Icons.add),
+            ),
+        ],
+      ),
       body: Column(
         children: [
           const Text('The Chart'),

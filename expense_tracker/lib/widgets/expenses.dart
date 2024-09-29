1316c06ee8155a739ec47ffbec2c55ce02bd2681
jyroball <jyrojimenez@gmail.com>
2024-09-28 17:01:54 -0700

    added undo deletion

--- expense_tracker/lib/widgets/expenses.dart
+++ expense_tracker/lib/widgets/expenses.dart
@@ -44,13 +44,38 @@ class _ExpensesState extends State<Expenses> {
   }
 
   void _removeExpense(Expense expense) {
+    //take not of index we are removing just in case we want to add it back
+    final expenseIndex = _registeredExpenses.indexOf(expense);
+
     setState(() {
       _registeredExpenses.remove(expense);
     });
+
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        duration: const Duration(seconds: 3),
+        content: const Text('Expense deleted.'),
+        action: SnackBarAction(
+          label: 'Undo', 
+          onPressed: () {
+            setState(() {
+              _registeredExpenses.insert(expenseIndex, expense);
+            });
+          },
+        )
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
+
+    Widget mainContent = Center(child: Text('No expense found. Start adding some!'));
+
+    if(_registeredExpenses.isNotEmpty) {
+      mainContent = ExpensesList(expenses: _registeredExpenses, onRemoveExpense: _removeExpense);
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Expense Tracker'),
@@ -65,7 +90,7 @@ class _ExpensesState extends State<Expenses> {
         children: [
           const Text('The Chart'),
           Expanded(
-            child: ExpensesList(expenses: _registeredExpenses, onRemoveExpense: _removeExpense),
+            child: mainContent,
           ),
         ],
       ),
